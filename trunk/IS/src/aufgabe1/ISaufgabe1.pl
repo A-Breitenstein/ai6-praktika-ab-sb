86d6f178199ed3f8453e426ec34ce83efd1c4148
breitenstein.alexander@googlemail.com <breitenstein.alexander@googlemail.com@26d1c594-7731-1177-1c24-03c564c58c6e>
2014-03-18 21:04:45 +0000

    ISaufgabe1.pl datei hinzugefügt

---
+++ trunk/IS/src/aufgabe1/ISaufgabe1.pl
@@ -0,0 +1,57 @@
+% Autor:
+% Datum: 18.03.2014
+
+%
+peter.
+ingeborg.
+hans.
+alex.
+sven.
+sabine.
+laura.
+alina.
+anna.
+berta.
+brigitte.
+charline.
+christiane.
+dorothea.
+
+vater(peter,hans).
+vater(peter,alex).
+vater(peter,sven).
+
+vater(hans,christiane).
+vater(hans,dorothea).
+
+vater(alex,brigitte).
+vater(alex,anna).
+
+vater(sven,sabine).
+vater(sven,berta).
+
+mutter(ingeborg,hans).
+mutter(ingeborg,alex).
+mutter(ingeborg,sven).
+
+mutter(charline,christiane).
+mutter(charline,dorothea).
+
+mutter(laura,brigitte).
+mutter(laura,anna).
+
+mutter(alina,sabine).
+mutter(alina,berta).
+
+oma(Oma,Kind):- vater(X,Kind),mutter(Oma,X).
+oma(Oma,Kind):- mutter(X,Kind),mutter(Oma,X).
+
+opa(Opa,Kind):- vater(X,Kind),vater(Opa,X).
+opa(Opa,Kind):- mutter(X,Kind),vater(Opa,X).
+
+
+
+
+
+
+
