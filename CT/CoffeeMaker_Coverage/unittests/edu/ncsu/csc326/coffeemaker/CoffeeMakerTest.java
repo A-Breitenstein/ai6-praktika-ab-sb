4920060feb81770e29f0498d4fc474d33ca20125
sven.bartel90 <sven.bartel90@26d1c594-7731-1177-1c24-03c564c58c6e>
2014-04-26 19:00:26 +0000

    CT aufgabe 2: bisschen coverage getestet, geht mit IntelliJ Ultimate version, diese hat EMMA auch.
    Rechtsklick auf den CoffeeMakerTest und Run "CoffeeMakerTest" with Coverage, dann oben in der Runconfiguration (links neben play button) gibt es einen reiter CodeCoverage und dort kann man EMMA auswählen, sollte mit JAVA 1.6 gemacht werden da sonst irgendwas bugt.

--- CT/CoffeeMaker_Coverage/unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ CT/CoffeeMaker_Coverage/unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -27,4 +27,40 @@ public class CoffeeMakerTest extends TestCase {
 		assertTrue(cm.addRecipe(r1));
 	}
 
+    public void testEnoughIngredients(){
+        assertTrue(cm.makeCoffee(r1,50) == 0);
+    }
+    public void testGetRecipeForName() {
+        assertEquals(r1.getName(),cm.getRecipeForName(r1.getName()));
+    }
+
+    public void testAddInventoryNothing() {
+        cm.addInventory(0, 0, 0, 0);
+    }
+
+    public void testEditRecipe(){
+        Recipe r3 = new Recipe();
+        r3.setName("Latte");
+        r3.setAmtChocolate(0);
+        r3.setAmtCoffee(3);
+        r3.setAmtMilk(3);
+        r3.setAmtSugar(1);
+        r3.setPrice(100);
+
+        cm.addRecipe(r1);
+        assertTrue(cm.editRecipe(r1, r3));
+        assertTrue(cm.editRecipe(r3, r1));
+    }
+
+    public void testDeleteRecipe() {
+        cm.deleteRecipe(r1);
+        assertFalse(cm.deleteRecipe(r1));
+    }
+
+    public void testInventoryToString() {
+        cm.checkInventory().toString();
+    }
+    public void testGetRecipes() {
+        cm.getRecipes();
+    }
 }
