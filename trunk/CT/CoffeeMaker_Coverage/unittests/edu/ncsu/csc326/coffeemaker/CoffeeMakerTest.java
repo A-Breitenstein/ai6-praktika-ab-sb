a973d4ba58d074fbd9355c95c5679979a88b66a6
breitenstein.alexander <breitenstein.alexander@26d1c594-7731-1177-1c24-03c564c58c6e>
2014-04-29 07:47:36 +0000

    CT aufgabe 2: coverage vom coffeemaker auf 100%

--- trunk/CT/CoffeeMaker_Coverage/unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ trunk/CT/CoffeeMaker_Coverage/unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -8,7 +8,7 @@ import junit.framework.TestCase;
 public class CoffeeMakerTest extends TestCase {
 	private CoffeeMaker cm;
 	private Inventory i;
-	private Recipe r1;
+	private Recipe r1,r2,r3,r4,r5;
 	
 	public void setUp() {
 		cm = new CoffeeMaker();
@@ -21,10 +21,70 @@ public class CoffeeMakerTest extends TestCase {
 		r1.setAmtMilk(1);
 		r1.setAmtSugar(1);
 		r1.setAmtChocolate(0);
+
+        r2 = new Recipe();
+		r2.setName("Rezept2");
+		r2.setPrice(50);
+		r2.setAmtCoffee(6);
+		r2.setAmtMilk(1);
+		r2.setAmtSugar(1);
+		r2.setAmtChocolate(0);
+
+        r3 = new Recipe();
+        r3.setName("Rezept3");
+        r3.setPrice(50);
+        r3.setAmtCoffee(6);
+        r3.setAmtMilk(1);
+        r3.setAmtSugar(1);
+        r3.setAmtChocolate(0);
+
+        r4 = new Recipe();
+        r4.setName("Rezept4");
+        r4.setPrice(50);
+        r4.setAmtCoffee(6);
+        r4.setAmtMilk(1);
+        r4.setAmtSugar(1);
+        r4.setAmtChocolate(0);
+
+        r5 = new Recipe();
+        r5.setName("Rezept5");
+        r5.setPrice(50);
+        r5.setAmtCoffee(10000);
+        r5.setAmtMilk(1);
+        r5.setAmtSugar(1);
+        r5.setAmtChocolate(0);
 	}
-	
+
+    public void testBezahlZuWenig() {
+        cm.addRecipe(r1);
+        assertTrue(cm.makeCoffee(r1, 0) == 0);
+    }
+
+    public void testZuWenigZutaten() {
+        cm.addRecipe(r5);
+        assertTrue(cm.makeCoffee(r5, 100) == 100);
+    }
+
 	public void testAddRecipe() {
 		assertTrue(cm.addRecipe(r1));
+	}
+    public void testAddRecipedouble() {
+		assertTrue(cm.addRecipe(r1));
+		assertFalse(cm.addRecipe(r1));
+	}
+    public void testAddRecipeMoreThanMax() {
+		assertTrue(cm.addRecipe(r1));
+		assertTrue(cm.addRecipe(r2));
+		assertTrue(cm.addRecipe(r3));
+		assertTrue(cm.addRecipe(r4));
+		assertFalse(cm.addRecipe(r5));
+	}
+    public void testAddRecipeEditMax() {
+		assertTrue(cm.addRecipe(r1));
+		assertTrue(cm.addRecipe(r2));
+		assertTrue(cm.addRecipe(r3));
+		assertTrue(cm.addRecipe(r4));
+		assertFalse(cm.editRecipe(r1,r1));
 	}
 
     public void testEnoughIngredients(){
@@ -67,16 +127,19 @@ public class CoffeeMakerTest extends TestCase {
     }
     public void testGetRecipeForName() {
         cm.addRecipe(r1);
-        assertEquals(r1.getName(),cm.getRecipeForName(r1.getName()).getName());
+        assertEquals(r1.getName(), cm.getRecipeForName(r1.getName()).getName());
     }
 
     public void testAddInventoryNothing() {
-        cm.addInventory(0, 0, 0, 0);
+        assertTrue(cm.addInventory(0, 0, 0, 0));
+    }
+    public void testAddInventorfalsePositiveSugar() {
+        assertTrue(cm.addInventory(0, 0, 1, 0));
     }
 
     public void testEditRecipe(){
         Recipe r3 = new Recipe();
-        r3.setName("Latte");
+        r3.setName("Coffee");
         r3.setAmtChocolate(0);
         r3.setAmtCoffee(3);
         r3.setAmtMilk(3);
@@ -116,9 +179,17 @@ public class CoffeeMakerTest extends TestCase {
         assertFalse(r1.equals(r2));
     }
 
-    public void testDeleteRecipe() {
-        cm.deleteRecipe(r1);
+    public void testEditRecipeIn() {
+        cm.addRecipe(r1);
+        assertTrue(cm.editRecipe(r1, r1));
+    }
+
+    public void testDeleteRecipeDouble() {
         assertFalse(cm.deleteRecipe(r1));
+    }
+    public void testDeleteRecipe() {
+        cm.addRecipe(r1);
+        assertTrue(cm.deleteRecipe(r1));
     }
 
     public void testInventoryToString() {
